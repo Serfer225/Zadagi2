6d465bef1b0f868d16032c9d596f2bf767f6a4a9
Serfer225 <serf0428@gmail.com>
2022-02-17 19:57:17 +0200

    Zadagi2

---
+++ main.js
@@ -0,0 +1,40 @@
+//Задача 8.1
+// function display_message() {
+//     let a = `Сообщение пришло на сайт`
+//     alert(a)
+// }
+// display_message()
+
+//Задача 8.2
+// let book = prompt("Введите свою любимую книгу")
+// function favorite_book(){
+//     alert(`Ваша любимая книга ${book}`)
+// }
+// favorite_book()
+
+//Задача 8.3
+// let razmer = prompt("Введите размер футболки.")
+// let name = prompt("Введите натпись на футболке")
+// function make_shirt() {
+//     alert(`В корзине футболка с размером ${razmer} и надписью ${name}`)
+// }
+// make_shirt()
+
+//Задача 8.4
+// function make_shirt(a) {
+//     let razmer = prompt("Введите размер футболки.")
+//     let name = prompt("Введите натпись на футболке")
+//     alert(`В корзине футболка с размером ${razmer} и надписью ${name}`)       Непонял как сделать         
+// }
+// let a = ("L")
+// let b = ("I like JavaScript")
+// make_shirt(a,b)
+
+//Задача 8.5
+// let country = prompt("Введите страну в которой живете")
+// let city = prompt("Введите город в которой живете")
+// function discribe_city(){
+//     alert(`Вы живете в ${city}е которое нахoдиться в ${country}`)
+// }
+// discribe_city()
+alert("Смотрите в коде задачи")
